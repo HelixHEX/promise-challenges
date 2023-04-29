37dc5e8628973c7ad0780944cb817118c7d06924
Elias <e.wambugu192@gmail.com>
2023-04-29 11:32:56 -0700

    finished challenge 2

--- challenges/challenge2.js
+++ challenges/challenge2.js
@@ -35,45 +35,77 @@
   * Asynchronously returns a greeting for a specified name.
   * @param name The name of the person to greet.
   */
-function greet(name) {
-    return new Promise(function(resolve, reject) {
-      setTimeout(function() {
-        if (typeof name === 'string') { 
-          resolve('Hello there, ' + name);
-        } else {
-          reject('Name must be a string!');
-        }
-      }, 1000);
-    });
+ function greet(name) {
+  return new Promise(function(resolve, reject) {
+    setTimeout(function() {
+      if (typeof name === 'string') { 
+        resolve('Hello there, ' + name);
+      } else {
+        reject('Name must be a string!');
+      }
+    }, 1000);
+  });
 }
 
 /**
- * Returns the uppercased version of a string.
- * @param {*} str The string to uppercase.
- */
+* Returns the uppercased version of a string.
+* @param {*} str The string to uppercase.
+*/
 function uppercaser(str) {
-    return new Promise(function(resolve, reject) {
-        setTimeout(function() {
-        if (typeof str === 'string') {
-            resolve(str.toUpperCase());
-        } else {
-            reject('Argument to uppercaser must be string');
-        }
-        }, 1500);
-    });
+  return new Promise(function(resolve, reject) {
+      setTimeout(function() {
+      if (typeof str === 'string') {
+          resolve(str.toUpperCase());
+      } else {
+          reject('Argument to uppercaser must be string');
+      }
+      }, 1500);
+  });
+}
+
+const spacer = (str) => {
+return new Promise((resolve, reject) => {
+  setTimeout(() => {
+    if (typeof str === 'string') {
+      resolve(str.split('').join(' '))
+    } else {
+      reject('Argument to spacer must be string')
+    }
+  }, 2000)
+})
 }
 
 name = 'Ducky'
 my_str = 'Make School is Awesome!!!'
+let foo = 'foo'
 
 greet(name)
-    .then((greetResult) => {
-        console.log(greetResult)
-        return uppercaser(my_str);
-    })
-    .then((uppercaserResult) => {
-        console.log(uppercaserResult)
-    }).catch((err) => {
-        console.log('Received an error!')
-        console.log(err);
-    });
+  .then((greetResult) => {
+      console.log(greetResult)
+      return uppercaser(my_str);
+  })
+  .then((uppercaserResult) => {
+      console.log(uppercaserResult)
+      return spacer(foo)
+  })
+  .then((spacerResult) => {
+    console.log(spacerResult)
+  })
+  .catch((err) => {
+      console.log('Received an error!')
+      console.log(err);
+  });
+
+/* ANSWERS */
+
+/* 1. The output will be 
+Hello there, Ducky
+MAKE SCHOOL IS AWESOME!!!
+*/
+
+/* 2. If greet() fails, it will print an error and uppercaser() will not run.
+
+/* 3. If greet() succeed and uppercaser() fails, it will print out the greet and then print
+Received an error!
+Argument to uppercaser must be string
+*/
